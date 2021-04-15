0974bcfdea72047701e2e21027a79b8fd9008f98
Harsh Agarwal <agarwalharsh244@gmail.com>
2021-04-15 11:27:56 +0530

    added tracker

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -8,7 +8,7 @@ import os
 import sys
 import pandas as pd
 import numpy as np
-from sklearn.metrics import mean_squared_error,mean_absolute_error, r2_score
+from sklearn.metrics import accuracy_score,confusion_matrix,classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from get_data import read_params
@@ -17,10 +17,10 @@ import joblib
 import json   
 
 def eval_metrics(actual, pred):
-    rmse =0
-    mae = mean_absolute_error(actual, pred)
-    r2 = r2_score(actual, pred)
-    return rmse, mae, r2
+    accuracy =accuracy_score(actual,pred)
+    cm = confusion_matrix(actual, pred)
+    cr = classification_report(actual, pred)
+    return accuracy, cm, cr
 
 def train_and_evaluate(config_path):
     config=read_params(config_path)
@@ -47,12 +47,35 @@ def train_and_evaluate(config_path):
 
     predicted_qualities = rf.predict(test_x)
     
-    (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
+    (acc, cm, rf) = eval_metrics(test_y, predicted_qualities)
 
     print("Random forest model (n_estimator=%f, max_feature=%f):" % (n_estimator, max_feature))
-    print("  RMSE: %s" % rmse)
-    print("  MAE: %s" % mae)
-    print("  R2: %s" % r2)
+    print("  RMSE: %s" % acc)
+    print("  MAE: %s" % cm)
+    print("  R2: %s" % rf)
+
+    scores_file=config["reports"]["scores"]
+    params_file=config["reports"]["params"]
+
+    with open(scores_file,"w") as f:
+        scores={
+            "acc":acc
+            
+        }
+        json.dump(scores, f, indent=4)
+    
+    with open(params_file,"w") as f:
+        params={
+            "n_estimator":n_estimator,
+            "max_feature":max_feature
+            }
+
+        json.dump(params, f, indent=4)
+    
+    os.makedirs(model_dir,exist_ok=True)
+    model_path=os.path.join(model_dir,"model.joblib")
+    joblib.dump(rf,model_path)
+
 
 if __name__=="__main__":
     args=argparse.ArgumentParser()
