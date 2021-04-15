da91ae68abbaeeeea3822b3eb7e6e7db58c4a395
Harsh Agarwal <agarwalharsh244@gmail.com>
2021-04-15 12:35:39 +0530

    created tox file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages 
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="It a employee arreition package",
+    author="Harsh Agarwal",
+    packages=find_packages(),
+    license="MIT"
+)
